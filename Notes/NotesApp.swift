af1047b1157b938538de969951b9ad2a9a2f55fa
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-13 14:42:50 +0530

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Jeet Dholakia on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
