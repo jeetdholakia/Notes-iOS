d73d0a880ff4e92cf8e933bedf9db815bfb07c72
Jeet Dholakia <onemediasoft@gmail.com>
2022-11-19 16:23:10 +0530

    Add delete request

--- Notes/NotesView.swift
+++ Notes/NotesView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct NotesView: View {
+    
+    @StateObject var notesViewModel = NotesViewModel()
+    @State private var showingAlert = false
+    @State private var selectedNoteId = "0"
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("Hello")
+                List(notesViewModel.notes, id: \.self.id) { note in
+                    Text(note.note)
+                        .swipeActions(content: {
+                            Button("Delete") {
+                                showingAlert.toggle()
+                                selectedNoteId = note.id
+                            }
+                        })
+                }.confirmationDialog("Select a color", isPresented: $showingAlert, titleVisibility: .visible) {
+                    Button("Delete") {
+                        Task {
+                            do {
+                                try await notesViewModel.deleteNote(id: selectedNoteId)
+                            } catch {
+                                
+                            }
+                        }
+                    }
+                    Button("Cancel") {
+                        
+                    }
+                }
             }.navigationTitle("Notes")
                 .toolbar(content: {
                     ToolbarItem(content: {
@@ -20,6 +46,12 @@ struct NotesView: View {
                         }
                     })
                 })
+        }.task {
+            do {
+                try await notesViewModel.fetchNotes()
+            } catch {
+                
+            }
         }
     }
 }
